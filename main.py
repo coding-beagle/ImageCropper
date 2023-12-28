13fb48fd38cae1cc0faac701b607a63ac8594867
coding-beagle <nicholasp.teague@gmail.com>
2023-12-28 17:36:21 +0000

    Converted to use classes

--- main.py
+++ main.py
@@ -5,192 +5,207 @@ from PIL import Image, ImageDraw
 import numpy as np
 import cv2 as cv
 
-img_path = ''
-can_show = False
-image_to_show = None
-selected_image = None
 
-accepted_extensions = ["jpg", "png", "bmp"] 
-
-def selectPath():
-    global img_path
-    global image_to_show
-    global selected_image
-    global img_cpy
-
-    selected_image = None
-    currdir = os.getcwd()
-    img_path = str(filedialog.askopenfile(initialdir=currdir))
-    if(img_path):
-        if(image_to_show):  # reset if an image has been selected already
-            image_to_show.destroy()
-        selected_image = img_path.split("name=")[1]
-        selected_image = selected_image.split(" mode=")[0]
-        selected_image = selected_image.strip("'")
-        img_path = selected_image
-        file_extension = selected_image.split(".")[-1]
-        file_extension = file_extension.strip("'")
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.accepted_extensions = ["bmp", "jpg", "png"]
         
-        if(file_extension in accepted_extensions):  # check for correct file type
-            with Image.open(selected_image) as img:
-                img_cpy = np.asarray(img)
-            updateImage()
+        self.img_ctk_object = None
+        self.img_label_object = None  # initialise
+        self.img_cpy = None
+        self.img_object = None
+
+        self.geometry("500x600")
+        self.title("Image Cropper")
+        self.resizable(False, False)
+    
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=0)
+        self.rowconfigure(0, weight=0)
+        self.rowconfigure(1, weight=0)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=0)
+    
+        self.label = ctk.CTkLabel(master=self, text="Image Cropper", font=("Roboto", 24))
+        self.label.grid(column=0, row=0,ipady=10, ipadx=0, padx=0, pady=10, sticky='ew', columnspan=3)
+    
+        self.button = ctk.CTkButton(self, text="Select Input Image", command=self.selectPath)
+        self.button.grid(row=1,column=0,ipady=0, ipadx=10, padx=20, pady=0)    
+        self.button_clear = ctk.CTkButton(self, text="Clear Image", command=self.clearImage)
+        self.button_clear.grid(row=1,column=2, ipady=0, ipadx=10, padx=20,pady=0)    
+    
+        self.slider_frame = SliderFrame(self, self.updateImage)
+        self.slider_frame.grid(row=3,column=0,padx=10, pady=5, columnspan = 1, sticky="ewns")
+    
+        self.frame_outputButtons = OutputButtonsFrame(self)
+        self.frame_outputButtons.grid(row=3,column=2,padx=10, pady=5, columnspan = 1, sticky="ewns")
+    
+
+    def selectPath(self):
+        self.curr_dir = os.getcwd()
+        self.img_object = filedialog.askopenfile(initialdir=self.curr_dir)
+        if(self.img_object is None):
+            return
         else:
-             print("Selected file is not an accepted filetype!")
-             filedialog.askopenfile(initialdir=currdir)
-    else:
-        selectPath()
+            self.file_extension = self.img_object.name.split(".")[-1]   # get file extension
+        
+            if(self.file_extension in self.accepted_extensions):  # check for correct file type
+                self.img_path = self.img_object.name
+                self.frame_outputButtons.setImgPath(self.img_path)
+                with Image.open(self.img_path) as self.img:
+                    self.img = np.asarray(self.img)   # convert to CV2 format and colours (BGR)
+                    self.img = cv.cvtColor(self.img, cv.COLOR_RGB2BGR)
+                self.updateImage()
 
 
-def selectOutputPath():
-    global outputPath
-    currdir = os.getcwd()
-    outputPath = str(filedialog.askdirectory(initialdir=currdir))
-    output_dir_label = ctk.CTkLabel(frame_outputButtons, text=outputPath, font=("Roboto", 12))
-    output_dir_label.grid(row=1,column=0,sticky="ew")
+    def clearImage(self):
+        self.img_label_object.destroy()
+        self.img_label_object = None
+        self.img_ctk_object = None
+        self.img_object = None
 
 
-def updateImage(arg=None):      #todo cleanup globals
-    nothing_burger = arg
-    global slider_starting_x
-    global slider_starting_y
-    global img_cpy
-    global selected_image
-    global image_to_show
-    global back_to_pil
-    global x0,y0,x1,y1
+    def updateImage(self):
+        if(self.img_object is None):    # checking if no image has been selected
+            return
+        else:
+            self.img_cpy = self.img.copy()
 
-    if selected_image is None:
-        return
+            self.img_size_y, self.img_size_x, self.img_size_colours = np.shape(self.img_cpy)
+            self.ratio = self.img_size_x / self.img_size_y
 
-    try:
-        scaleY, scaleX, colors = np.shape(img_cpy)
-    except NameError:
-        return
-    ratio = scaleX/scaleY
+            self.max_rect_size = min(self.img_size_x, self.img_size_y)  # we want the maximum size of the cropping square to be the minimum of the two image dimensions
 
-    if(ratio > 1):
-        max_rect_size = scaleY
-    else:
-        max_rect_size = scaleX
+            self.rectsize = self.max_rect_size * self.slider_frame.getSizeVal() / 100
+            
+            self.slider_frame.updateSliderEndstops(self.slider_frame.slider_starting_x, int(self.img_size_x-self.rectsize))
+            self.slider_frame.updateSliderEndstops(self.slider_frame.slider_starting_y, int(self.img_size_y-self.rectsize))
+
+            self.x0 = int(self.slider_frame.getXVal())       # coordinates to draw cropping square
+            self.y0 = int(self.slider_frame.getYVal())
+            self.x1 = int(self.x0 + self.rectsize)
+            self.y1 = int(self.y0 + self.rectsize)
+
+            if(self.x1 > self.img_size_x):
+                self.x1 = self.img_size_x - 1
+            if(self.y1 > self.img_size_y):
+                self.y1 = self.img_size_y - 1
+
+            self.thickness = int(self.max_rect_size / 1000) * 10    # scale the thickness of the cropping square with the size of image
+            
+            cv.rectangle(self.img_cpy, (self.x0, self.y0), (self.x1, self.y1), (255, 0, 0), self.thickness)
+
+            self.img_pil_format = cv.cvtColor(self.img_cpy, cv.COLOR_BGR2RGB)
+            self.img_pil_format = Image.fromarray(self.img_pil_format)
+            
+            if(self.img_ctk_object is not None):
+                self.img_ctk_object.configure(dark_image=self.img_pil_format)
+            else:
+                self.img_ctk_object = ctk.CTkImage(dark_image=self.img_pil_format, size=(200*self.ratio,200))
+
+            if(self.img_label_object is not None):
+                self.img_label_object.configure(image=self.img_ctk_object)
+            else:
+                self.img_label_object = ctk.CTkLabel(self, image=self.img_ctk_object, text="")
+                self.img_label_object.grid(column=0, row=2, padx=10, pady=5,columnspan=3, sticky='ew')
     
-    image_cvitised = np.asarray(img_cpy)
-    image_cvitised = cv.cvtColor(image_cvitised, cv.COLOR_RGB2BGR)
 
-    rect_size = max_rect_size*slider_size.get()/100
+    def getCropCoords(self):
+        return(self.x0, self.y0, self.x1, self.y1)       
+
+
+class SliderFrame(ctk.CTkFrame):
+    def __init__(self, master, slider_callback):
+        super().__init__(master)
+        self.slider_callback = slider_callback      # we pass in the updateImage function to update the image whenever values are updated
+
+        self.size_val = 50
+        self.x_val = 0
+        self.y_val = 0
+
+        self.slider_size_label = ctk.CTkLabel(self, text="Select size of rectangle:", font=("Roboto", 12))
+        self.slider_size_label.grid(row=0,column=0,sticky="ew")
+        self.slider_size = ctk.CTkSlider(self, from_=0, to=100, command=self.updateSizeVal)
+        self.slider_size.grid(row=1,column=0,sticky="ew")
     
-    slider_starting_x.configure(to=int((scaleX-rect_size)))
-    slider_starting_y.configure(to=int((scaleY-rect_size)))
-
-    x0 = slider_starting_x.get()
-    y0 = slider_starting_y.get()
-    x1 = x0 + rect_size
-    y1 = y0 + rect_size
-
+        self.slider_starting_x_label = ctk.CTkLabel(self, text="Select x position of rectangle:", font=("Roboto", 12))
+        self.slider_starting_x_label.grid(row=2,column=0,sticky="ew")
+        self.slider_starting_x = ctk.CTkSlider(self, from_=0, to=100, command=self.updateXVal)
+        self.slider_starting_x.grid(row=3,column=0,sticky="ew")
+    
+        self.slider_starting_y_label = ctk.CTkLabel(self, text="Select y position of rectangle:", font=("Roboto", 12))
+        self.slider_starting_y_label.grid(row=4,column=0,sticky="ew")
+        self.slider_starting_y = ctk.CTkSlider(self, from_=0, to=100, command=self.updateYVal)
+        self.slider_starting_y.grid(row=5, column=0,sticky="ew")
     
 
-    if(x1 > scaleX):
-        x1 = scaleX - 1
-    if(y1 > scaleY):
-        y1 = scaleY - 1
+    def updateSizeVal(self, value):         # could probably combine into one function
+        self.size_val = value
+        self.slider_callback()
 
-    thickness = int(max_rect_size / 1000) * 10
+
+    def getSizeVal(self):
+        return self.size_val
     
-    cv.rectangle(image_cvitised, (int(x0), int(y0)), (int(x1),int(y1)), (255), thickness)
 
-    back_to_pil = cv.cvtColor(image_cvitised, cv.COLOR_BGR2RGB)
-    back_to_pil = Image.fromarray(back_to_pil)
+    def updateXVal(self, value):
+        self.x_val = value
+        self.slider_callback()
 
-    selected_image = ctk.CTkImage(dark_image=back_to_pil, size=(200*ratio,200))
+    def getXVal(self):
+        return self.x_val
     
-    if(image_to_show is not None):
-        image_to_show.configure(image=selected_image)
-    else:
-        image_to_show = ctk.CTkLabel(root, image=selected_image, text="")
-        image_to_show.grid(column=0, row=2, padx=10, pady=5,columnspan=3, sticky='ew')
 
-# todo, fix sliders causing program not to work after clearing img
-def clearImage():
-    try:
-        global image_to_show
-        global selected_image
-        image_to_show.destroy()
-        selected_image.destroy()
-        selected_image = None
-        image_to_show = None
+    def updateYVal(self, value):
+        self.y_val = value
+        self.slider_callback()
 
-    except AttributeError:
-        pass
+    def getYVal(self):
+        return self.y_val
+    
+
+    def updateSliderEndstops(self, slider, newEndstop):
+        slider.configure(to=newEndstop)
 
 
-def getCroppedImg():
-    global img_path
-    output = ""
-    try:
-        with Image.open(img_path) as img:
-            im_crop = img.crop((x0, y0, x1, y1))
-            im_resized = im_crop.resize((240,240))
-            output = outputPath + "/img.bmp" 
-            im_resized.save(output, "BMP")
-            # im_resized_cropped = im_resized.crop((0, 70, 240, 139))
-            # output2 = outputPath + "/imgCropped.bmp" 
-            # im_resized_cropped.save(output2, "BMP")
-        output = "Successful Write!"
-    except AttributeError:
-        output = "Bad Write!"
-    output_label = ctk.CTkLabel(frame_outputButtons, text=output, font=("Roboto", 12))
-    output_label.grid(row=3,column=0,sticky="ew")
+class OutputButtonsFrame(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
 
+        self.button_askoutput = ctk.CTkButton(self, text="Set Output Directory", command=self.selectOutputPath)
+        self.button_askoutput.grid(row=0,column=0,ipady=0, ipadx=10, padx=20, pady=0)    
+        self.button_getoutput = ctk.CTkButton(self, text="Generate Output", command=self.getCroppedImg)
+        self.button_getoutput.grid(row=2,column=0, ipady=0, ipadx=10, padx=20,pady=0)
+    
+    def selectOutputPath(self):
+        self.currdir = os.getcwd()
+        self.outputPath = str(filedialog.askdirectory(initialdir=self.currdir))
+        self.output_dir_label = ctk.CTkLabel(self, text=self.outputPath, font=("Roboto", 12))
+        self.output_dir_label.grid(row=1,column=0,sticky="ew")
+
+
+    def setImgPath(self, path):
+        self.img_path = path
+
+
+    def getCroppedImg(self):
+        output = ""
+        try:
+            with Image.open(self.img_path) as img:
+                im_crop = img.crop(app.getCropCoords())
+                im_resized = im_crop.resize((240,240))
+                output = self.outputPath + "/img.bmp" 
+                im_resized.save(output, "BMP")
+            output = "Successful Write!"
+        except AttributeError:
+            output = "Bad Write!"
+        output_label = ctk.CTkLabel(self, text=output, font=("Roboto", 12))
+        output_label.grid(row=3,column=0,sticky="ew")
+        
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
-
-root = ctk.CTk()
-root.geometry("500x600")
-root.title("Image Cropper")
-root.resizable(False, False)
-
-root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
-root.columnconfigure(2, weight=0)
-root.rowconfigure(0, weight=0)
-root.rowconfigure(1, weight=0)
-root.rowconfigure(2, weight=1)
-root.rowconfigure(3, weight=0)
-
-label = ctk.CTkLabel(master=root, text="Image Cropper", font=("Roboto", 24))
-label.grid(column=0, row=0,ipady=10, ipadx=0, padx=0, pady=10, sticky='ew', columnspan=3)
-
-button = ctk.CTkButton(root, text="Select Input Image", command=selectPath)
-button.grid(row=1,column=0,ipady=0, ipadx=10, padx=20, pady=0)    
-button_clear = ctk.CTkButton(root, text="Clear Image", command=clearImage)
-button_clear.grid(row=1,column=2, ipady=0, ipadx=10, padx=20,pady=0)    
-
-frame_sliders = ctk.CTkFrame(root)
-
-slider_size_label = ctk.CTkLabel(frame_sliders, text="Select size of rectangle:", font=("Roboto", 12))
-slider_size_label.grid(row=0,column=0,sticky="ew")
-slider_size = ctk.CTkSlider(frame_sliders, from_=0, to=100, command=updateImage)
-slider_size.grid(row=1,column=0,sticky="ew")
-
-slider_starting_x_label = ctk.CTkLabel(frame_sliders, text="Select x position of rectangle:", font=("Roboto", 12))
-slider_starting_x_label.grid(row=2,column=0,sticky="ew")
-slider_starting_x = ctk.CTkSlider(frame_sliders, from_=0, to=100, command=updateImage)
-slider_starting_x.grid(row=3,column=0,sticky="ew")
-
-slider_starting_y_label = ctk.CTkLabel(frame_sliders, text="Select y position of rectangle:", font=("Roboto", 12))
-slider_starting_y_label.grid(row=4,column=0,sticky="ew")
-slider_starting_y = ctk.CTkSlider(frame_sliders, from_=0, to=100, command=updateImage)
-slider_starting_y.grid(row=5, column=0,sticky="ew")
-
-frame_sliders.grid(row=3,column=0,padx=10, pady=5, columnspan = 1, sticky="ewns")
-
-frame_outputButtons = ctk.CTkFrame(root)
-button_askoutput = ctk.CTkButton(frame_outputButtons, text="Set Output Directory", command=selectOutputPath)
-button_askoutput.grid(row=0,column=0,ipady=0, ipadx=10, padx=20, pady=0)    
-button_getoutput = ctk.CTkButton(frame_outputButtons, text="Generate Output", command=getCroppedImg)
-button_getoutput.grid(row=2,column=0, ipady=0, ipadx=10, padx=20,pady=0)
-
-frame_outputButtons.grid(row=3,column=2,padx=10, pady=5, columnspan = 1, sticky="ewns")
-
-root.mainloop()
+app = App()
+app.mainloop()
