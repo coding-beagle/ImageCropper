fixed bugs with rotation

main.py
@@ -1,7 +1,7 @@
 import os
 from tkinter import filedialog
 import customtkinter as ctk
-from PIL import Image, ImageDraw
+from PIL import Image
 import numpy as np
 import cv2 as cv
 
@@ -20,11 +20,9 @@ class App(ctk.CTk):
         self.title("Image Cropper")
         self.resizable(False, False)
     
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
+        self.columnconfigure((0,1), weight=1)
         self.columnconfigure(2, weight=0)
-        self.rowconfigure(0, weight=0)
-        self.rowconfigure(1, weight=0)
+        self.rowconfigure((0,1), weight=0)
         self.rowconfigure(2, weight=1)
         self.rowconfigure(3, weight=0)
     
@@ -33,13 +31,15 @@ class App(ctk.CTk):
     
         self.button = ctk.CTkButton(self, text="Select Input Image", command=self.selectPath)
         self.button.grid(row=1,column=0,ipady=0, ipadx=10, padx=20, pady=0)    
-        self.button_clear = ctk.CTkButton(self, text="Clear Image", command=self.clearImage)
-        self.button_clear.grid(row=1,column=2, ipady=0, ipadx=10, padx=20,pady=0)    
+        
+
+        self.image_button_frame = ImageButtonFrames(self, self.clearImage, self.rotateCCW, self.rotateCW)
+        self.image_button_frame.grid(row=1,column=2, ipady=0, ipadx=10, padx=20,pady=0)    
     
         self.slider_frame = SliderFrame(self, self.updateImage)
         self.slider_frame.grid(row=3,column=0,padx=10, pady=5, columnspan = 1, sticky="ewns")
     
-        self.frame_outputButtons = OutputButtonsFrame(self)
+        self.frame_outputButtons = OutputButtonsFrame(self, self.writeImage)
         self.frame_outputButtons.grid(row=3,column=2,padx=10, pady=5, columnspan = 1, sticky="ewns")
     
 
@@ -61,6 +61,8 @@ class App(ctk.CTk):
 
 
     def clearImage(self):
+        if(self.img_label_object is None):
+            return
         self.img_label_object.destroy()
         self.img_label_object = None
         self.img_ctk_object = None
@@ -74,7 +76,9 @@ class App(ctk.CTk):
             self.img_cpy = self.img.copy()
 
             self.img_size_y, self.img_size_x, self.img_size_colours = np.shape(self.img_cpy)
-            self.ratio = self.img_size_x / self.img_size_y
+            
+            self.ratio = self.img_size_x / self.img_size_y            
+    
 
             self.max_rect_size = min(self.img_size_x, self.img_size_y)  # we want the maximum size of the cropping square to be the minimum of the two image dimensions
 
@@ -95,15 +99,16 @@ class App(ctk.CTk):
 
             self.thickness = int(self.max_rect_size / 1000) * 10    # scale the thickness of the cropping square with the size of image
             
-            cv.rectangle(self.img_cpy, (self.x0, self.y0), (self.x1, self.y1), (255, 0, 0), self.thickness)
+            cv.rectangle(self.img_cpy, (self.x0, self.y0), (self.x1-self.thickness, self.y1-self.thickness), (255, 0, 0), self.thickness)
 
             self.img_pil_format = cv.cvtColor(self.img_cpy, cv.COLOR_BGR2RGB)
             self.img_pil_format = Image.fromarray(self.img_pil_format)
             
-            if(self.img_ctk_object is not None):
+            if(self.img_ctk_object is not None):                    # don't create new image or label if one already exists
                 self.img_ctk_object.configure(dark_image=self.img_pil_format)
+                self.img_ctk_object.configure(size=(200*self.ratio,200))    # a bit wasteful but whatever
             else:
-                self.img_ctk_object = ctk.CTkImage(dark_image=self.img_pil_format, size=(200*self.ratio,200))
+                self.img_ctk_object = ctk.CTkImage(dark_image=self.img_pil_format, size=(200*self.ratio,200))       # ratio is used to fix the size of the displayed image
 
             if(self.img_label_object is not None):
                 self.img_label_object.configure(image=self.img_ctk_object)
@@ -113,7 +118,37 @@ class App(ctk.CTk):
     
 
     def getCropCoords(self):
-        return(self.x0, self.y0, self.x1, self.y1)       
+        return(self.x0, self.y0, self.x1, self.y1)
+
+
+    def rotateCCW(self):
+        try:
+            self.img = cv.rotate(self.img, cv.ROTATE_90_COUNTERCLOCKWISE)
+            self.updateImage()
+        except AttributeError:
+            pass
+
+    def rotateCW(self):
+        try:
+            self.img = cv.rotate(self.img, cv.ROTATE_90_CLOCKWISE)
+            self.updateImage()
+        except AttributeError:
+            pass
+
+
+    def writeImage(self):
+        self.output = ""
+        try:
+            self.im_crop = cv.cvtColor(self.img, cv.COLOR_BGR2RGB)
+            self.im_crop = Image.fromarray(self.im_crop)
+            self.im_crop = self.im_crop.crop(self.getCropCoords())
+            self.im_resized = self.im_crop.resize((240,240))
+            self.output = self.frame_outputButtons.getOutputPath() + "/img.bmp" 
+            self.im_resized.save(self.output, "BMP")
+            self.output = "Successful Write!"
+        except AttributeError:
+            self.output = "Bad Write!"      
+        return self.output
 
 
 class SliderFrame(ctk.CTkFrame):
@@ -171,8 +206,10 @@ class SliderFrame(ctk.CTkFrame):
 
 
 class OutputButtonsFrame(ctk.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, writeImgCallback):
         super().__init__(master)
+
+        self.writeImageCallback = writeImgCallback
 
         self.button_askoutput = ctk.CTkButton(self, text="Set Output Directory", command=self.selectOutputPath)
         self.button_askoutput.grid(row=0,column=0,ipady=0, ipadx=10, padx=20, pady=0)    
@@ -190,20 +227,31 @@ class OutputButtonsFrame(ctk.CTkFrame):
         self.img_path = path
 
 
+    def getOutputPath(self):
+        return self.outputPath
+
+
     def getCroppedImg(self):
-        output = ""
-        try:
-            with Image.open(self.img_path) as img:
-                im_crop = img.crop(app.getCropCoords())
-                im_resized = im_crop.resize((240,240))
-                output = self.outputPath + "/img.bmp" 
-                im_resized.save(output, "BMP")
-            output = "Successful Write!"
-        except AttributeError:
-            output = "Bad Write!"
+        output = self.writeImageCallback()
         output_label = ctk.CTkLabel(self, text=output, font=("Roboto", 12))
         output_label.grid(row=3,column=0,sticky="ew")
         
+
+class ImageButtonFrames(ctk.CTkFrame):
+    def __init__(self, master, clearCallback, rotateCCWCallback, rotateCWCallback):
+        super().__init__(master)
+
+        self.clearCallback = clearCallback
+        self.rotateCWCallback = rotateCWCallback
+        self.rotateCCWCallback = rotateCCWCallback
+
+        self.button_clear = ctk.CTkButton(self, text="×", command=self.clearCallback, width=5)
+        self.button_clear.grid(row=0, column=2, ipadx=7, padx=2, sticky="w")
+        self.button_rotateCCW = ctk.CTkButton(self, text="↶", command=self.rotateCCWCallback, width=5)
+        self.button_rotateCCW.grid(row=0, column=0, ipadx=5, padx=(19,2),sticky="w")
+        self.button_rotateCW = ctk.CTkButton(self, text="↷", command=self.rotateCWCallback, width=5)
+        self.button_rotateCW.grid(row=0, column=1, ipadx=5, padx=2,sticky="w")
+
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")